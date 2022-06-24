aed1881ebd4f668fb1f660ce1271b23805dd92eb
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-06-24 17:49:29 -0400

    added notification component, useeffect in app js for cart request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-
+import { useEffect } from 'react';
 import {useSelector} from 'react-redux'
 
 
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible)
+  const cart = useSelector(state=> state.cart)
+
+  useEffect(() => {
+    fetch('https://react-http-afa43-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  }, [cart])
 
   return (
     <Layout>
