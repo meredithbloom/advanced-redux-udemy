70c312a7ee6c496a21179d3c7f2842d9872d742b
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-06-24 16:19:27 -0400

    added cart slice. add to cart and remove from cart actions included

--- src/app/cart-slice.js
+++ src/app/cart-slice.js
@@ -11,14 +11,31 @@ const cartSlice = createSlice({
       const newItem = action.payload
       const existingItem = state.items.find(item => item.id === newItem.id)
       if (!existingItem) {
-        state.items.push()
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        })
+      } else {
+        existingItem.quantity++
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price
       }
     },
     removeFromCart(state, action) {
-      state.items = state.items - action.payload
+      const id = action.payload
+      const existingItem = state.items.find(item => item.id === id)
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id)
+      } else {
+        existingItem.quantity--
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price
+      }
     }
   }
 })
 
 export const cartActions = cartSlice.actions
 
+export default cartSlice
--- src/app/store.js
+++ src/app/store.js
@@ -1,26 +1,6 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
-// import counterReducer from '../features/counter/counterSlice';
 import uiSlice from './ui-slice'
-
-const initialCartState = { items: {}, showCart: false }
-
-const cartSlice = createSlice({
-  name: 'cart',
-  initialState: initialCartState,
-  reducers: {
-    toggleCart(state) {
-      state.showCart = !state.showCart
-    },
-    addToCart(state, action) {
-      state.items = state.items + action.payload
-    },
-    removeFromCart(state, action) {
-      state.items = state.items - action.payload
-    }
-  }
-})
-
-export const cartActions = cartSlice.actions
+import cartSlice from './cart-slice';
 
 
 const store = configureStore({
@@ -30,9 +10,5 @@ const store = configureStore({
   }
 })
 
-// export const store = configureStore({
-//   reducer: {
-//     counter: counterReducer,
-//   },
-// });
+
 export default store
