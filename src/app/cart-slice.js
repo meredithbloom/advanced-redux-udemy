3aa7a2271a4e5bf326effe911292ebd6836807fb
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-08-10 16:01:08 -0400

    advanced redux another module

--- src/app/cart-slice.js
+++ src/app/cart-slice.js
@@ -1,16 +1,24 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-
-const initialCartState = { items: [], totalQuantity: 0 }
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialState: initialCartState,
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    changed: false
+  },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addToCart(state, action) {
       const newItem = action.payload
       const existingItem = state.items.find(item => item.id === newItem.id)
       state.totalQuantity++
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -28,6 +36,7 @@ const cartSlice = createSlice({
       const id = action.payload
       const existingItem = state.items.find(item => item.id === id)
       state.totalQuantity--
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter(item => item.id !== id)
       } else {
@@ -38,6 +47,15 @@ const cartSlice = createSlice({
   }
 })
 
-export const cartActions = cartSlice.actions
 
-export default cartSlice
+
+
+
+
+
+
+
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
