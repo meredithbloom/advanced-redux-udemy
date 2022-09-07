23b0d93dbfdc8c0bc1437d3a932a26262288bb98
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-09-07 12:53:20 -0400

    minor edit to dummy products

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -5,16 +5,22 @@ import classes from './Products.module.css';
 const DUMMY_PRODUCTS = [
   {
     id: 'p1',
-    price: 6,
+    price: 10,
     title: 'My first book',
     description: 'The first book I ever wrote.'
   },
   {
     id: 'p2',
-    price: 5,
+    price: 12,
     title: 'My second book',
     description: 'Better than the first.'
-  }  
+  },
+  {
+    id: 'p3',
+    price: 14,
+    title: 'My third book',
+    description: 'Critics say I could have held off on writing this one.'
+  }
 ]
 
 
